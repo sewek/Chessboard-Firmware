3f6d48d681f037046d19f682232b5499a5be147a
Seweryn Malczewski <seweryn.malczewski@gmail.com>
2024-08-16 06:07:33 +0200

    refactor(chess_logic): Types declarations

--- lib/chess_logic/include/chess_types.h
+++ lib/chess_logic/include/chess_types.h
@@ -8,12 +8,12 @@
 #include <stdbool.h>
 #endif
 
-using chess_piece_color_t = enum {
+enum class chess_piece_color_t {
   kWhite = 0,
   kBlack,
 };
 
-using chess_piece_type_t = enum {
+enum class chess_piece_type_t {
   kPawn = 0,
   kKnight,
   kBishop,
@@ -22,7 +22,7 @@ using chess_piece_type_t = enum {
   kKing,
 };
 
-using chess_file_t = enum {
+enum class chess_file_t {
   kA = 1,
   kB,
   kC,
@@ -33,7 +33,7 @@ using chess_file_t = enum {
   kH,
 };
 
-using chess_move_type_t = enum {
+enum class chess_move_type_t {
   kNormal = 0x0,
   kCapture = 0x1,
   kCastling = 0x2,
@@ -43,34 +43,34 @@ using chess_move_type_t = enum {
   kEnPassant = 0x20,
 };
 
-using chess_game_state_t = enum {
+enum class chess_game_state_t {
   kGameNotStarted = 0,
   kGameInProgress,
   kGameEnded,
 };
 
-using chess_game_result_t = enum {
+enum class chess_game_result_t {
   kResultWhiteWins = 0,
   kResultBlackWins,
   kResultDraw,
 };
 
-using chess_piece_t = struct {
+typedef struct {
   chess_piece_color_t color;
   chess_piece_type_t type;
-};
+} chess_piece_t;
 
-using chess_position_t = struct {
+typedef struct {
   chess_file_t file;
   uint8_t rank;
-};
+} chess_position_t;
 
-using chess_move_t = struct {
+typedef struct {
   chess_position_t from;
   chess_position_t to;
   chess_move_type_t type;
   chess_piece_t piece;
-};
+} chess_move_t;
 
 enum class ChessEventType {
   GameStarted = 0,
